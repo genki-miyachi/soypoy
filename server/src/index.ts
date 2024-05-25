e4341263a9a9ec83089dfdb0673c094bc141c7fc
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2024-05-25 23:32:51 +0900

    Add ESLint, Prettier for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
 
-const startServer = async() => {
+const startServer = async () => {
   const typeDefs = gql`
     type Query {
       hello: String
@@ -10,12 +10,13 @@ const startServer = async() => {
 
   const resolvers = {
     Query: {
-      hello: () => 'Hello world!',
+      hello: () => "Hello world!",
     },
   };
   // FIXME: Express と Apollo sever の version 競合で Type error が発生する
   // 同様の Issue が報告されているものの解決せず。
   // https://github.com/apollographql/apollo-server/issues/4592
+  // eslint-disable-next-line
   const app: any = express();
   const server = new ApolloServer({ typeDefs, resolvers });
 
@@ -23,8 +24,8 @@ const startServer = async() => {
   server.applyMiddleware({ app });
 
   app.listen({ port: 4000 }, () =>
-    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`),
   );
-}
+};
 
 startServer();
